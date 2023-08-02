20f4e64058236e0dfed3b311846ae2b330d4282c
MrUnwonted <arjungodzarjun@gmail.com>
2023-08-03 01:53:03 +0530

    API fetched

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import Navbar from "./Components/NavBar/Navbar";
 import "./App.css"
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
+import {originals,action,trending,comedy,horror,romance,documentaries } from './urls'
 
 function App() {
   return (
     <div className="App">
      <Navbar />
      <Banner />
-     <RowPost />
+     <RowPost url={originals} title='Netflif Originals'/>   
+      <RowPost url={trending} title='Trending' isSmall/>   
+      <RowPost url={action} title='Actions' isSmall/>   
+      <RowPost url={comedy} title='Comedy Movies' isSmall/>   
+      <RowPost url={horror} title='Horror Movies' isSmall/>   
+      <RowPost url={romance} title='Romance Movies' isSmall/>   
+      <RowPost url={documentaries} title='Documentaries' isSmall/>  
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3'
+export const API_KEY = 'b6055e1a0b40fd591d27b3d12fb13ae3'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
